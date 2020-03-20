3958ad02277fc06d81012ec5c7c53414d6adb090
uSources <hate463@gmail.com>
2020-03-20 14:02:45 +0100

    Luces y spawner

---
+++ Scenes/PoinLight.gd
@@ -0,0 +1,69 @@
+extends Node2D
+
+export var speed: float
+export var radius: float
+
+
+var check_bounds: bool
+
+var angle: float
+var direction: Vector2
+var player
+
+signal damage
+
+func _ready():
+	randomize()
+	_get_player()
+	_set_angle()
+	connect("damage",player,"_on_damage")
+	pass
+
+func _get_player():
+	player = get_tree().get_nodes_in_group("Player")[0]
+	pass
+func _process(delta):
+	if check_bounds:
+		_check_screen_bounds()
+	
+	_check_distance()
+	_move(delta)
+	pass
+	
+func _set_angle():
+	direction =	(player.global_position - self.global_position).normalized()
+	pass
+	
+func _check_distance():
+	var player_distance = player.global_position.distance_to(self.global_position)
+	if player_distance < radius:
+		$RayCast2D.set_cast_to(player.global_position - self.global_position)
+		if not $RayCast2D.is_colliding():
+			emit_signal("damage", player_distance)
+	pass
+func _check_screen_bounds():
+	var window_size = OS.get_window_size()
+	if global_position.y > window_size.y:
+		direction = direction.bounce(Vector2.DOWN)
+	if global_position.y < 0:
+		direction = direction.bounce(Vector2.UP)
+	if global_position.x > window_size.x:
+		direction = direction.bounce(Vector2.LEFT)
+	if global_position.x < 0:
+		direction = direction.bounce(Vector2.RIGHT)
+	pass
+
+func _move(delta):
+	position += direction * speed * delta
+	pass
+
+func _on_Area2D_area_entered(area):
+	var difference: Vector2 = self.global_position - area.global_position
+	direction = direction.bounce(difference.normalized())
+	pass # Replace with function body.
+
+
+func _on_VisibilityNotifier2D_viewport_entered(viewport):
+	yield(get_tree().create_timer(1), "timeout")
+	check_bounds = true
+	pass # Replace with function body.
---
+++ Scenes/Spawer.gd
@@ -0,0 +1,36 @@
+extends Node2D
+
+export var spaw_distance: float
+export var light: PackedScene
+
+enum DIRECTIONS{UP, DOWN, LEFT, RIGHT}
+
+func _ready():
+	randomize()
+	pass
+
+func _on_Timer_timeout():
+	_spawn_light()
+	pass
+
+func _spawn_light():
+	var viewport_size = get_viewport().size
+	var obj = light.instance()
+	var direction = DIRECTIONS.values()[randi() % DIRECTIONS.size()]
+	var spawn_pos
+
+	if direction == DIRECTIONS.UP:
+		spawn_pos = Vector2(rand_range(0, viewport_size.x), - spaw_distance)
+	
+	elif direction == DIRECTIONS.DOWN:
+		spawn_pos = Vector2(rand_range(0, viewport_size.x), viewport_size.y + spaw_distance)	
+	
+	elif direction == DIRECTIONS.LEFT:
+		spawn_pos = Vector2(- spaw_distance, rand_range(0, viewport_size.y))
+	
+	elif direction == DIRECTIONS.RIGHT:
+		spawn_pos = Vector2(viewport_size.x + spaw_distance, rand_range(0, viewport_size.y))
+	
+	obj.global_position = spawn_pos
+	add_child(obj)
+	pass
